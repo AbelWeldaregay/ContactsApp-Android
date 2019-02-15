0c8db88c2f8cc77b0039fb818ae1e0b07605aeb5
Abel Weldaregay <abelweldaregay@gmail.com>
2019-02-15 11:01:10 -0500

    initial commit

---
+++ app/src/main/java/com/cs541/abel/contactsapp/Activites/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cs541.abel.contactsapp.Activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cs541.abel.contactsapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
